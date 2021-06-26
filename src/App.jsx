a114530147c80320b2d35fb98dfd411adaa7a972
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-26 17:29:45 +0900

    useStateの利用

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage'
 
 // reactを定義する関数を生成する
 const App = () => {
 
-    const onClickButton = () => alert();
+    const onClickCountUp = () => {
+        // 引数に設定したい値をセットする
+        setNum(num + 1);
+    };
+
+    // 第一変数:stateに使用する変数
+    // 第二変数:stateを変更する関数名
+    // useStateの引数で初期値を設定できる
+    const [num,setNum] = useState(0);
 
     // Jsx記法：JavaScriptの中でreturnし、HTMLを記載していくこと
     // returnするHTMLのタグは一つのタグで囲まないといけないというルールがある
@@ -21,7 +29,9 @@ const App = () => {
                 元気です！
             </ColorfulMessage>
 
-            <button onClick = {onClickButton}>ボタン</button>
+            <button onClick = {onClickCountUp}>カウントアップ</button>
+
+            <p>{num}</p>
         </>
     );
 };
