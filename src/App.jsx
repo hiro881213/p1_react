d5cbf8bf1704e4bfaa2b49a6fe9b34449de9154e
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-26 19:17:00 +0900

    useEffectの使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import React,{ useState } from 'react';
+/* eslint react-hooks/exhaustive-deps: off */
+import React,{ useEffect, useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage'
 
 // reactを定義する関数を生成する
 const App = () => {
 
+    // 第一変数:stateに使用する変数
+    // 第二変数:stateを変更する関数名
+    // useStateの引数で初期値を設定できる
+    const [num,setNum] = useState(0);
+    const [faceShowFlag,setFaceShowFlag] = useState(true);
+
+    /**
+     * num更新処理
+     */
     const onClickCountUp = () => {
         // 引数に設定したい値をセットする
         setNum(num + 1);
     };
 
-    // 第一変数:stateに使用する変数
-    // 第二変数:stateを変更する関数名
-    // useStateの引数で初期値を設定できる
-    const [num,setNum] = useState(0);
+    /**
+     * faceShowFlag更新処理
+     */
+    const onClickSwitchShowFlag = () => {
+        setFaceShowFlag(!faceShowFlag);
+    }
+
+    // useEffctの第二引数を[]にすると初回のみ実行される
+    // useEffctの第二引数を変数にするとその変数変更時にのみ実行される
+    useEffect(() => {
+        // numが3の倍数の時だけ顔文字を表示する
+        if (num > 0) {
+            if (num % 3 === 0) {
+                faceShowFlag || setFaceShowFlag(true);
+            } else {
+                faceShowFlag && setFaceShowFlag(false);
+            }    
+        }
+    },[num]);
+
 
     // Jsx記法：JavaScriptの中でreturnし、HTMLを記載していくこと
     // returnするHTMLのタグは一つのタグで囲まないといけないというルールがある
@@ -30,8 +56,11 @@ const App = () => {
             </ColorfulMessage>
 
             <button onClick = {onClickCountUp}>カウントアップ</button>
-
+            <br />
+            <button onClick = {onClickSwitchShowFlag}>on/off</button>
             <p>{num}</p>
+
+            {faceShowFlag && <p>٩( ᐛ )و</p>}
         </>
     );
 };
