2665749040b221473daea910dbe21afb5386ee11
TomohiroFurukawa <godbard@TomohiroFurukawa-no-MacBook-Air.local>
2021-06-26 16:47:50 +0900

    イベント呼び出し、スタイル設定

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import React from 'react';
 // reactを定義する関数を生成する
 const App = () => {
 
+    const onClickButton = () => alert();
+
+    // スタイルオブジェクト
+    const contentStyle = {
+        color: 'blue',
+        fontSize: '18px'
+    };
+
     // Jsx記法：JavaScriptの中でreturnし、HTMLを記載していくこと
     // returnするHTMLのタグは一つのタグで囲まないといけないというルールがある
     // <>(<React.Fragment>)で囲むことで余計なタグを設定しなくて良くなる
     return(
+        /* オブジェクトとしてスタイルを書いていく */
+        /* {}でJavaScriptのコードを利用できる */
         <>
-            <h1>こんにちは！</h1>
-            <p>お元気ですか？</p>
+            <h1 style = {{color :  'red'}}>こんにちは！</h1>
+            <p style = {contentStyle} >お元気ですか？</p>
+            <button onClick = {onClickButton}>ボタン</button>
         </>
     );
 };
